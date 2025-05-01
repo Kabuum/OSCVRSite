54d7be1143cfabef0f5d7653f91abb51ed6112b4
Kabuum <thomsenribermartin@gmail.com>
2025-05-01 18:39:41 +0200

    testing ngrok

--- frontend/src/ColorPicker.jsx
+++ frontend/src/ColorPicker.jsx
@@ -8,7 +8,8 @@ export function ColorPicker(){
     const socketRef = useRef(null);
 
     useEffect(() => {
-        socketRef.current = io("wss://ba03-87-72-205-225.ngrok-free.app");
+        socketRef.current = io("wss://e4b6-87-72-205-225.ngrok-free.app");
+        console.log("attempted websocket connection");
 
         return () => {
             socketRef.current.disconnect();
